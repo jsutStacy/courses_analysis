1f4018556533672a8caa6586ca01aa7604ffcf56
Ragnar <ragnarvent@gmail.com>
2016-10-09 21:43:58 +0300

    Extension counter improvements

--- analysis/ExtensionCounter.py
+++ analysis/ExtensionCounter.py
@@ -9,24 +9,38 @@ import operator
 
 class ExtensionCounter(object):
 
+    def __init__(self):
+        self.blacklist = ['be', 'html']
+
     def print_all_extensions(self):
         extensions = sorted(self.__count_ext().items(), key=operator.itemgetter(1), reverse=True)
         for k, v in extensions:
             print k, v
 
-    @staticmethod
-    def __count_ext():
+    def __count_ext(self):
         extensions = {}
-        lectures = Lecture.select()
+        lectures = Lecture.select().where(Lecture.content == '')
         for lecture in lectures:
-            if lecture.path == "html2txt":
-                continue
+            clean_url = None
+            if lecture.url.startswith('https'):
+                clean_url = lecture.url[8:]
+            elif lecture.url.startswith('http'):
+                clean_url = lecture.url[7:]
+
             ext = lecture.name.split('.')[-1]
+            if ext in self.blacklist \
+                    or (not ext.isalpha()) \
+                    or (not clean_url)  \
+                    or clean_url.find('/') < 0\
+                    or ext.strip() == '' \
+                    or lecture.name.find('.') < 0:
+                continue
+
             if ext in extensions:
                 extensions[ext] += 1
             else:
                 extensions[ext] = 1
-        return extensions
+        return {k: v for k, v in extensions.iteritems() if v > 1 and len(k) < 5}
 
 if __name__ == '__main__':
     ExtensionCounter().print_all_extensions()
